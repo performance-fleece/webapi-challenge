0358264f1e6d32cde0a8dd59fba6b1c3cc202c3c
Jeremy Morales <jeremy.morales@gmail.com>
2019-08-03 09:29:29 -0700

    server.js created

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const projectRouter = require('./project/projectRouter');
+const actionRouter = require('./action/actionRouter');
+
+const server = express();
+server.use(logger);
+server.use(express.json());
+
+server.use('/api/action', actionRouter);
+server.use('/api/project', projectRouter);
+
+function logger(req, res, next) {
+  const time = new Date();
+  console.log(`${req.method} to ${req.path} at ${time.toISOString()}`);
+}
